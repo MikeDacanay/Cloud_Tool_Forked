305c71ff9c87c576e4acfdd6375a647c3edbc238
EKerrigan <erin@kerrigan.family>
2018-10-08 14:44:07 -0500

    css

--- bin_dev/js/eloqua.js
+++ bin_dev/js/eloqua.js
@@ -12,48 +12,15 @@ function validateFields() {
         $('input[name=firstName]').addClass("error");
         incomplete = true;
     }
-    if ($('input[name=company]').val() == "") {
-        $('input[name=company]').addClass("error");
-        incomplete = true;
-    }
-    if ($('input[name=jobTitle]').val() == "") {
-        $('input[name=jobTitle]').addClass("error");
-        incomplete = true;
-    }
+
     if ($('select[name=country]').val() == "Select Country" || $('select[name=country]').val() == "" || $('select[name=country]').val() == "None Selected") {
         $('#field17').addClass("error");
         incomplete = true;
     }
 
-    if ($('input[name=address]').val() == "") {
-        $('input[name=address]').addClass("error");
-        incomplete = true;
-    }
-
-    if ($('select[name=stateOrProvince]').val() == "Select State" || $('select[name=stateOrProvince]').val() ==null)  {
-
-        $('#field15').addClass("error");
-        incomplete = true;
-    }
-    if ($('input[name=city]').val() == "") {
-        $('input[name=city]').addClass("error");
-        incomplete = true;
-    }
 
     if ($('input[name=lastName]').val() == "") {
         $('input[name=lastName]').addClass("error");
-        incomplete = true;
-    }
-    if ($('input[name=phoneNumber]').val() == "") {
-        $('input[name=phoneNumber]').addClass("error");
-        incomplete = true;
-    }
-    if ($('input[name=phoneNumber]').val() == "") {
-        $('input[name=phoneNumber]').addClass("error");
-        incomplete = true;
-    }
-    if ($('input[name=zipPostalCode]').val() == "") {
-        $('input[name=zipPostalCode]').addClass("error");
         incomplete = true;
     }
 
@@ -71,9 +38,11 @@ function validateFields() {
 
     }
     else {
-        $('#form-popup').bPopup().close();
+      //  $('#form-popup').bPopup().close();
+//THIS IS WHERE RESULTS GO
+        document.getElementById("field18").value="";
 
-        document.getElementById("field18").value="Customer considers themselves "+familiarity_dataviz+" familiar with Data Visualization.  They " +familiarity_oracle+" an Oracle account. They "+oracle_customer+" a current Oracle customer. Job role is: " + role_text+ ".";
+
         //testing
         // $('#form196').submit();
 
