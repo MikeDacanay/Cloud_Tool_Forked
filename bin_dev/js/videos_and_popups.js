b707062bedad8cdb8fd91e0cf43a87a57faded48
Mike Dacanay <migd64@gmail.com>
2018-11-21 14:55:39 -0800

    added vid player to desktop/tablet

--- bin_dev/js/videos_and_popups.js
+++ bin_dev/js/videos_and_popups.js
@@ -69,45 +69,49 @@ function openYouTubeOverlay() {
         position: ['auto', 'auto'],
         escClose: 'true'
     });
-
-
     $("#youtube-overlay-content").css('opacity', 1);
 }
 
 function openVideoOverlay(ob) {
     var videoID=ob;
-
   //  ob.data ? videoID = ob.data.video : videoID = $(ob.currentTarget).attr("vid");
     var cwidth = 639;
-    var cheight = 414;
-
-    if(window.innerWidth<500){
+    var cheight = 404;
+    if(window.innerWidth<768){
        cwidth=300;
        cheight=168;
 
     }
-    $("#voverlay-container").css({
-        'display': 'block',
-        'width': cwidth + 'px',
-        'height': 'auto'
-    });
-    $("#voverlay-content").css({
-        'display': 'block',
-        'width': cwidth + 'px',
-        'height': 'auto'
-    });
-//2017 player
-    $("#voverlay-content").html(
-            '<video data-account="1460825906" data-player="SyGHte2Ul" data-embed="default" data-video-id="' + videoID + '" class="video-js" controls style="width:100%;height: 100%;position:relative;top: 0px; bottom: 0px; right: 0px; left: 0px;"></video>'+
-            '<script src="//players.brightcove.net/1460825906/SyGHte2Ul_default/index.min.js"></script>'
-    );
+
+    if(window.innerWidth>766){
+        $("#voverlay-container").css({
+            'display': 'block',
+            'width': cwidth + 'px',
+            // 'height': 'auto',
+            'height': (cheight+75) + 'px',
+        });
+        $("#voverlay-content").css({
+            'display': 'block',
+            'width': cwidth + 'px',
+            'height': cheight + 'px',
+        });
+    //2017 player
+        $("#voverlay-content").html(
+                '<video data-account="1460825906" data-player="SyzXcjbW8f" data-embed="default" data-video-id="' + videoID + '" class="video-js" controls style="width:100%;height: 100%;position:relative;top: 0px; bottom: 0px; right: 0px; left: 0px;"></video>'+
+                '<script src="//players.brightcove.net/1460825906/SyzXcjbW8f_default/index.min.js"></script>'
+        );
 
 
-    $("#voverlay-container").bPopup({
-        follow: [true, true],
-        position: ['auto', 'auto'],
-        appendTo:'html'
-    });
+        $("#voverlay-container").bPopup({
+            follow: [true, true],
+            position: ['auto', 'auto'],
+            appendTo:'html'
+        });
+    }else{
+        window.open(
+          'https://video.oracle.com/detail/video/'+videoID // <- This is what makes it open in a new window.
+        );        
+    }
 }
 
 function openOverlay(pop){
